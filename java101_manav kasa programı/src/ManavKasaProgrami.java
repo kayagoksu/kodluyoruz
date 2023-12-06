0544497beb20745d8c4ee85e028a2365f836c877
kayagoksu <goksukaya92@gmail.com>
2023-12-06 23:06:51 +0300

    manav kasa programı

---
+++ java101_manav kasa programı/src/ManavKasaProgrami.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ManavKasaProgrami {
+    public static void main(String[] args) {
+        double armut, elma, domates, muz, patlican, toplamTutar;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Armut Kaç Kilo ? : ");
+        armut = input.nextDouble();
+        System.out.print("Elma Kaç Kilo ? : ");
+        elma = input.nextDouble();
+        System.out.print("Domates Kaç Kilo ? : ");
+        domates = input.nextDouble();
+        System.out.print("Muz Kaç Kilo ? : ");
+        muz = input.nextDouble();
+        System.out.print("Patlıcan Kaç Kilo ? : ");
+        patlican = input.nextDouble();
+
+        toplamTutar = (armut * 2.14) + (elma * 3.67) + (domates * 1.11) + (muz * 0.95) + (patlican * 5);
+
+        System.out.println("Toplam Tutar : " + toplamTutar);
+    }
+}
+
