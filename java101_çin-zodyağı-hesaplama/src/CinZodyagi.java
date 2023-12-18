7dee058a29c7b135e985facd50c074c7e377dac2
kayagoksu <goksukaya92@gmail.com>
2023-12-18 18:17:19 +0300

    çin zodyağı hesaplama

---
+++ java101_çin-zodyağı-hesaplama/src/CinZodyagi.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class CinZodyagi {
+    public static void main(String[] args) {
+        int date, kalan;
+        String burc="";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        date = input.nextInt();
+
+        kalan = date%12;
+
+        switch (kalan) {
+            case 0:
+                burc = "Maymun";
+                break;
+            case 1:
+                burc = "Horoz";
+                break;
+            case 2:
+                burc = "Köpek";
+                break;
+            case 3:
+                burc = "Domuz";
+                break;
+            case 4:
+                burc = "Fare";
+                break;
+            case 5:
+                burc = "Öküz";
+                break;
+            case 6:
+                burc = "Kaplan";
+                break;
+            case 7:
+                burc = "Tavşan";
+                break;
+            case 8:
+                burc = "Ejderha";
+                break;
+            case 9:
+                burc = "Yılan";
+                break;
+            case 10:
+                burc = "At";
+                break;
+            case 11:
+                burc = "Koyun";
+                break;
+            default:
+                burc = "Bulunamadı.";
+        }
+
+        System.out.println("Çin Zodyağı Burcunuz : " + burc);
+
+    }
+}
+
