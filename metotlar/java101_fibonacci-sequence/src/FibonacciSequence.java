3d69b8f9ffd54693994a37c68eb88b72c1c9d845
kayagoksu <goksukaya92@gmail.com>
2024-01-03 18:34:55 +0300

    update

--- metotlar/java101_fibonacci-sequence/src/FibonacciSequence.java
+++ metotlar/java101_fibonacci-sequence/src/FibonacciSequence.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class FibonacciSequence {
 
     static int fibonacci(int number) {
-        int res;
+        
         if (number == 1 || number == 2) {
             return 1;
         }
